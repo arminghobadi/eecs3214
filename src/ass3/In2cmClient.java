79d3ce23f8be8a318b4963ef486bd12ff1590459
ArminGh <armin.ghobadi95@gmail.com>
2018-03-19 17:43:43 -0400

    working version of assignment 3

--- src/ass3/In2cmClient.java
+++ src/ass3/In2cmClient.java
@@ -6,67 +6,39 @@ import java.net.*;
 public class In2cmClient {
 	
 	public static Socket socket = null;
-	public static DataInputStream dataInputStream = null;
-    public static DataOutputStream dataOutputStream = null;
-    public static BufferedReader bufferedReader = null;
+	public static DataInputStream din = null;
+    public static DataOutputStream dout = null;
     private static final String IP = "localhost";
     private static final int port = 29512; // IMPORTANT!: remember to change this!! its 20000 + the last 4 digits of your student number
-	public static boolean openConnection = true;
     
 	public static void main(String args[]) throws Exception {
 		
-		//first arg is inch
-		//send it to server
-		//server calculates to 2 decimal points
-		// client sends thanks
-		// server sends bye and close connection
 		System.out.println(args[0]);
 		if (args.length != 1) {
-			//throw new IllegalArgumentException("Usage: In2cmClient <a number in inches to convert to cm>");
+			throw new IllegalArgumentException("Usage: In2cmClient <a number in inches to convert to cm>");
 		}
-		
-		//BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		
-		//Socket clientSocket = new Socket("localhost",29512);
-		
-		
-		//DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		
-		//BufferedReader InFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		
 		socket = new Socket(IP,port);
 		
-		System.out.println("connecting...");
+        din = new DataInputStream(socket.getInputStream());
+        dout = new DataOutputStream(socket.getOutputStream());
+        String welcomeMsg = din.readUTF();
+		//System.out.println(welcomeMsg);
+		//System.out.println("connection established");
 		
+		//System.out.println("sending the number");
+		dout.writeUTF(args[0]);
+		dout.flush();
+	
+		String in2cm = din.readUTF();
+		//System.out.println(in2cm);
+		dout.writeUTF("Thanks");
+		dout.flush();
+		String goodbyeMsg = din.readUTF();
+		//System.out.println(goodbyeMsg);
+		//System.out.println("closing connectiong");
 		
-        dataInputStream = new DataInputStream(socket.getInputStream());
-        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        dataOutputStream = new DataOutputStream(socket.getOutputStream());
-		
-		while(openConnection) {
-			//dataInputStream.readLine();
-			System.out.println("4");
-			
-			//dataOutputStream.writeBytes("hey");
-			//bufferedReader.wait(1000);
-			String welcomeMsg = bufferedReader.readLine();
-			System.out.println("connection established");
-			if (!welcomeMsg.equals("Welcome")) {
-				System.out.println("something is wrong");
-			}
-			System.out.println("sending the number");
-			dataOutputStream.writeBytes(args[0]);
-			String result = bufferedReader.readLine();
-			System.out.println(result);
-			dataOutputStream.writeBytes("Thanks");
-			System.out.println("closing connectiong");
-			
-			dataInputStream.close();
-			dataOutputStream.close();
-			bufferedReader.close();
-			socket.close();
-			openConnection = false;
-			
-		}
+		din.close();
+		dout.close();
+		socket.close();
 	}
 }
--- src/ass3/In2cmServer.java
+++ src/ass3/In2cmServer.java
@@ -7,60 +7,44 @@ public class In2cmServer {
 	
 	public static ServerSocket serverSocket = null;
 	public static Socket socket = null;
-	public static BufferedReader bufferedReader = null;
-	public static DataOutputStream dataOutputStream = null;
+	public static DataOutputStream dout = null;
+	public static DataInputStream din = null;
 	private static final int port = 29512; // IMPORTANT!: remember to change this!! its 20000 + the last 4 digits of your student number
 	
 	public static void main( String argv[] ) throws Exception{
 		
 		serverSocket = new ServerSocket(port);
-		System.out.println("waiting...");
+		//System.out.println("waiting...");
+		socket = serverSocket.accept();
+		//System.out.println("connection accepted");
 		
+		dout = new DataOutputStream(socket.getOutputStream());
+		din = new DataInputStream(socket.getInputStream());
+		dout.flush();
+		dout.writeUTF("Welcome");
+		dout.flush();
 		
-		//ServerSocket welcomeSocket = new ServerSocket(29512);
+		//System.out.println("reading from client");
+		String in2cm = din.readUTF();
 		
-		while (true) {
-			//Socket connectionSocket = welcomeSocket.accept();
-			System.out.println("still waiting....");
-			socket = serverSocket.accept();
-			System.out.println("connection accepted");
-			
-			//a welcome msg
-			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			//BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			System.out.println("1");
-			dataOutputStream = new DataOutputStream(socket.getOutputStream());
-			//DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			System.out.println("2");
-			//System.out.println(bufferedReader.readLine());
-			dataOutputStream.writeBytes("Welcome");
-			dataOutputStream.flush();
-			System.out.println("3");
-			
-			String in2cm = bufferedReader.readLine();
-			System.out.println("reading from client");
-			
-			dataOutputStream.writeBytes("yayy!");
-			
-			String thankyou = bufferedReader.readLine();
-			
-			System.out.println("done sending");
-			
-			if (!thankyou.equals("Thanks")) {
-				System.out.println("something went wrong");
-			}
-			
-			System.out.println("closing connection");
-			
-			dataOutputStream.close();
-			bufferedReader.close();
-			socket.close();
-			serverSocket.close();
-			//clientSentence = inFromClient.readLine();
-			
-			//capitalizedSentence = clientSentence.toUpperCase();
-			
-			//outToClient.writeBytes(capitalizedSentence);
-		}
+		double ans = Double.parseDouble(in2cm);
+		ans = ans * 2.54;
+		int digits = (int) (ans * 100);
+		ans = digits / 100.0;
+		
+		dout.writeUTF(Double.toString(ans));
+		dout.flush();
+		
+		String thankyou = din.readUTF();
+		//System.out.println(thankyou);
+		
+		dout.writeUTF("Bye");
+		dout.flush();
+		
+		//System.out.println("done sending");
+		//System.out.println("closing connection");
+		dout.close();
+		socket.close();
+		serverSocket.close();
 	}
 }
