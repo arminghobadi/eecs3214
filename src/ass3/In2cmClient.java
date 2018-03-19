bcfe107beb32ef354e2505d330178f1e9e63a204
ArminGh <armin.ghobadi95@gmail.com>
2018-03-19 16:35:53 -0400

    code not working

--- src/ass3/In2cmClient.java
+++ src/ass3/In2cmClient.java
@@ -1,4 +1,4 @@
-package ass3;
+//package ass3;
 
 import java.io.*;
 import java.net.*;
@@ -8,39 +8,65 @@ public class In2cmClient {
 	public static Socket socket = null;
 	public static DataInputStream dataInputStream = null;
     public static DataOutputStream dataOutputStream = null;
-    private static final String IP = "35.197.28.71";
+    public static BufferedReader bufferedReader = null;
+    private static final String IP = "localhost";
     private static final int port = 29512; // IMPORTANT!: remember to change this!! its 20000 + the last 4 digits of your student number
-	
-	public static void main(String argv[]) throws Exception {
-		String sentence = null;
-		String modified;
+	public static boolean openConnection = true;
+    
+	public static void main(String args[]) throws Exception {
 		
 		//first arg is inch
 		//send it to server
 		//server calculates to 2 decimal points
 		// client sends thanks
 		// server sends bye and close connection
-		
-		if (argv.length != 2) {
-			throw new IllegalArgumentException("Usage: In2cmClient <a number in inches to convert to cm>");
+		System.out.println(args[0]);
+		if (args.length != 1) {
+			//throw new IllegalArgumentException("Usage: In2cmClient <a number in inches to convert to cm>");
 		}
 		
 		//BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		
-		Socket clientSocket = new Socket("localhost",29512);
+		//Socket clientSocket = new Socket("localhost",29512);
 		
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		
-		BufferedReader InFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		//DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		
-		//sentence = inFromUser.readLine();
+		//BufferedReader InFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		
-		outToServer.writeBytes(sentence);
+		socket = new Socket(IP,port);
 		
-		modified = InFromServer.readLine();
+		System.out.println("connecting...");
 		
-		System.out.println("from server:" + modified);
 		
-		clientSocket.close();
+        dataInputStream = new DataInputStream(socket.getInputStream());
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+		
+		while(openConnection) {
+			//dataInputStream.readLine();
+			System.out.println("4");
+			
+			//dataOutputStream.writeBytes("hey");
+			//bufferedReader.wait(1000);
+			String welcomeMsg = bufferedReader.readLine();
+			System.out.println("connection established");
+			if (!welcomeMsg.equals("Welcome")) {
+				System.out.println("something is wrong");
+			}
+			System.out.println("sending the number");
+			dataOutputStream.writeBytes(args[0]);
+			String result = bufferedReader.readLine();
+			System.out.println(result);
+			dataOutputStream.writeBytes("Thanks");
+			System.out.println("closing connectiong");
+			
+			dataInputStream.close();
+			dataOutputStream.close();
+			bufferedReader.close();
+			socket.close();
+			openConnection = false;
+			
+		}
 	}
 }
--- src/ass3/In2cmServer.java
+++ src/ass3/In2cmServer.java
@@ -1,27 +1,66 @@
-package ass3;
+//package ass3;
 
 import java.io.*;
 import java.net.*;
 
 public class In2cmServer {
-	public static void main( String argv[]) throws Exception{
-		String clientSentence;
-		String capitalizedSentence;
+	
+	public static ServerSocket serverSocket = null;
+	public static Socket socket = null;
+	public static BufferedReader bufferedReader = null;
+	public static DataOutputStream dataOutputStream = null;
+	private static final int port = 29512; // IMPORTANT!: remember to change this!! its 20000 + the last 4 digits of your student number
+	
+	public static void main( String argv[] ) throws Exception{
 		
-		ServerSocket welcomeSocket = new ServerSocket(29512);
+		serverSocket = new ServerSocket(port);
+		System.out.println("waiting...");
+		
+		
+		//ServerSocket welcomeSocket = new ServerSocket(29512);
 		
 		while (true) {
-			Socket connectionSocket = welcomeSocket.accept();
+			//Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("still waiting....");
+			socket = serverSocket.accept();
+			System.out.println("connection accepted");
 			
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			//a welcome msg
+			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			//BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			System.out.println("1");
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+			//DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			System.out.println("2");
+			//System.out.println(bufferedReader.readLine());
+			dataOutputStream.writeBytes("Welcome");
+			dataOutputStream.flush();
+			System.out.println("3");
 			
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			String in2cm = bufferedReader.readLine();
+			System.out.println("reading from client");
 			
-			clientSentence = inFromClient.readLine();
+			dataOutputStream.writeBytes("yayy!");
 			
-			capitalizedSentence = clientSentence.toUpperCase();
+			String thankyou = bufferedReader.readLine();
 			
-			outToClient.writeBytes(capitalizedSentence);
+			System.out.println("done sending");
+			
+			if (!thankyou.equals("Thanks")) {
+				System.out.println("something went wrong");
+			}
+			
+			System.out.println("closing connection");
+			
+			dataOutputStream.close();
+			bufferedReader.close();
+			socket.close();
+			serverSocket.close();
+			//clientSentence = inFromClient.readLine();
+			
+			//capitalizedSentence = clientSentence.toUpperCase();
+			
+			//outToClient.writeBytes(capitalizedSentence);
 		}
 	}
 }
